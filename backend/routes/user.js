a53d6831291ff9a0446bbc3ad3a11fcef010bc75
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-03-07 18:01:12 +0300

    Create user rout

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,1 +1,7 @@
 const express = require("express");
+const { createNewUser } = require("../controllers/user");
+const usersRouter = express.Router();
+/******************************************************************************** */
+usersRouter.post("/", createNewUser);
+
+module.exports = usersRouter;
