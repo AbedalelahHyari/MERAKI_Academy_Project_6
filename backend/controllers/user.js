b9bd998091344f24fdc4f7e33d6110c054ddfecc
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-03-07 18:00:37 +0300

    Create a function called createNewUser in user controller

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,1 +1,40 @@
 const userModel = require("../database/models/user");
+
+const createNewUser = (req, res) => {
+  const { name, email, password, age, gender, location, role } = req.body;
+  const newUser = new userModel({
+    name,
+    email,
+    password,
+    age,
+    gender,
+    location,
+    role,
+  });
+  newUser
+    .save()
+    .then((user) => {
+      res.status(201).json({
+        success: true,
+        message: `The User has been created Successfully`,
+        user: user,
+      });
+    })
+    .catch((err) => {
+      if (err.keyPattern) {
+        return res.status(409).json({
+          success: false,
+          message: `The email already exists`,
+        });
+      }
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err,
+      });
+    });
+};
+
+module.exports = {
+  createNewUser,
+};
