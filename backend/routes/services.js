3f6ce2956742a2959d4fb245303e63ce28b99017
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-03-07 19:27:03 +0300

    Create service rout

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -1,1 +1,7 @@
 const express = require("express");
+const { createNewService } = require("../controllers/services");
+const servicesRouter = express.Router();
+/******************************************************************************** */
+servicesRouter.post("/", createNewService);
+
+module.exports = servicesRouter;
