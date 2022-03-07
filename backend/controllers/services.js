182ab120dc384f41c17e15ba58e55b2f36c9484c
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-03-07 19:26:11 +0300

    Create new function called createNewService in the service controller

--- backend/controllers/services.js
+++ backend/controllers/services.js
@@ -1,1 +1,35 @@
 const servicesModel = require("../database/models/service");
+
+const createNewService = (req, res) => {
+  const { name, description } = req.body;
+  const newService = new servicesModel({
+    name,
+    description,
+  });
+  newService
+    .save()
+    .then((service) => {
+      res.status(201).json({
+        success: true,
+        message: `The service has been created Successfully`,
+        service: service,
+      });
+    })
+    .catch((err) => {
+      if (err.keyPattern) {
+        return res.status(409).json({
+          success: false,
+          message: `The service already exists`,
+        });
+      }
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err,
+      });
+    });
+};
+
+module.exports = {
+  createNewService,
+};
