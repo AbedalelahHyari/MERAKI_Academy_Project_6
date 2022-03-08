c648ae2610f9cab78bb60c3e5dded06ac15573fe
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-03-08 20:47:18 +0300

    Add condition on the App.js to show the component based of the user isLoggedIn

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,42 @@
 import "./App.css";
 import { Route, Router, Routes } from "react-router-dom";
 import React, { useState, useEffect, Suspense, lazy } from "react";
-
+import { useSelector } from "react-redux";
 import Home from "./components/Home/Home";
 import Registration from "./components/Registration/Registration";
-
-
+import SingleServicePage from "./components/SingleServicePage/SingleServicePage";
+import WorkerPage from "./components/WorkerPage/WorkerPage";
 
 function App() {
+  const state = useSelector((state) => {
+    return {
+      isLoggedIn: state.loginReducer.isLoggedIn,
+    };
+  });
   return (
     <>
-     <div className="App">
-          <div className="Home">
-            <Routes>
-            <Route path="/Home" element={<Home/>} />
-              {/* {state.isLoggedIn ? (
-                <Route path="/Home" element={<Home />} />
-              ) : (
-                <></>
-              )} */}
-            </Routes>
-          </div>
-          {/* {state.isLoggedIn ? <></> : <Register />} */}
-           <Registration/>
+      <div className="App">
+        <div className="Home">
+          <Routes>
+            {state.isLoggedIn ? (
+              <Route path="/Home" element={<Home />} />
+            ) : (
+              <></>
+            )}
+            {state.isLoggedIn ? (
+              <Route path="/services/:id" element={<SingleServicePage />} />
+            ) : (
+              <></>
+            )}
+            {state.isLoggedIn ? (
+              <Route path="/worker/:id" element={<WorkerPage />} />
+            ) : (
+              <></>
+            )}
+          </Routes>
         </div>
+        {state.isLoggedIn ? <></> : <Registration />}
+      </div>
     </>
   );
 }
