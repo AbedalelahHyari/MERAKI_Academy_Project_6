108a408d32f6f564728fed19afb641c2efbc3d7c
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-03-08 18:46:23 +0300

    Add registration and home component on App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
+import { Route, Router, Routes } from "react-router-dom";
+import React, { useState, useEffect, Suspense, lazy } from "react";
+import Register from "./components/Register/Register";
+import Home from "./components/Home/Home";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome To App</h1>
-    </div>
+    <>
+     <div className="App">
+          <div className="Home">
+            <Routes>
+            <Route path="/Home" element={<Home/>} />
+              {/* {state.isLoggedIn ? (
+                <Route path="/Home" element={<Home />} />
+              ) : (
+                <></>
+              )} */}
+            </Routes>
+          </div>
+          {/* {state.isLoggedIn ? <></> : <Register />} */}
+          { <Registration/>}
+        </div>
+    </>
   );
 }
 
