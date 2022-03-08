5da712f8d245ec1d21ef789b283b7dad272f7ce3
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-03-08 20:54:03 +0300

    Add SingleService Component and and create function getService by Id and show the service by looping in jsx element

--- frontend/src/components/SingleServicePage/SingleServicePage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "../SingleServicePage/SingleServicePage.css";
-const SingleServicePage = () => {
-  return <div>SingleServicePage</div>;
-};
-
-export default SingleServicePage;
