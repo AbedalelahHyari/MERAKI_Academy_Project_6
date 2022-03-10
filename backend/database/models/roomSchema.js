02b6aa9797786d85283f88484a5726b6e106d90c
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-03-10 21:19:29 +0300

    Update the roomSchema

--- backend/database/models/roomSchema.js
+++ backend/database/models/roomSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const roomSchema = new mongoose.Schema({
-  roomId: { type: mongoose.Schema.Types.ObjectId, ref: "Request" },
+  room_ID: { type: mongoose.Schema.Types.ObjectId, ref: "Request" },
 });
 
 module.exports = mongoose.model("Room", roomSchema);
