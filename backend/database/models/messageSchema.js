b1581935563408254af39d848b19db41a90a8007
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-03-10 21:20:28 +0300

    Update the messageSchema

--- backend/database/models/messageSchema.js
+++ backend/database/models/messageSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-  message: { type: String, required },
+  message: { type: String, required: true },
   room_id: { type: mongoose.Schema.Types.ObjectId, ref: "Room" },
   sender_id: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
