aa5d3709bb57762b519f3fbd9ab52db9a890dbd5
BasharAbuHannoud <abuhannoud.bashar@gmail.com>
2022-03-14 20:44:37 +0300

    add authentication to get worker profile

--- backend/routes/worker.js
+++ backend/routes/worker.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { workerProfile, getWorkerInfoById } = require("../controllers/worker");
 const workerRouter = express.Router();
 /******************************************************************************** */
-workerRouter.post("/:_id", workerProfile);
+const authentication = require("../middleware/authentication");
+/************************************************************* */
+workerRouter.post("/:_id", authentication,workerProfile);
 workerRouter.get("/:_id", getWorkerInfoById);
 
 module.exports = workerRouter;
